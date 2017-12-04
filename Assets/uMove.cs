2508f75759aea6f6bda50516caac866203cba2e3
Drata <adescudero93@gmail.com>
2017-12-05 00:10:44 +0100

    First Commit

---
+++ Assets/uMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uMove : MonoBehaviour {
+
+    public float velocidad = 0;
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += new Vector3(Input.GetAxis("Horizontal"),
+                                          0,
+                                          Input.GetAxis("Vertical")) * velocidad * Time.deltaTime;
+    }
+}
